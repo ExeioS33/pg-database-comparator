8aa13c8e5b854af6ce438b7d0ede22de4ba6d1d8
sanda.andria <sandria@groupecf.com>
2024-10-22 10:40:52 +0200

    remove bad files

--- db_operations/AbstractDatabaseOperations.py
+++
@@ -1,9 +0,0 @@
-# db_operations/AbstractDatabaseOperations.py
-
-from typing import Protocol
-
-
-class AbstractCopy(Protocol):
-    def execute_copy_command(self, *args, **kwargs) -> None:
-        """Execute the copy command for database operations."""
-        raise NotImplementedError("Subclasses must implement this method.")
